90322067594f86ed8edb7909f259c0abe159cd7a
SubhayanDas08 <nips.subhayan@gmail.com>
2021-10-24 11:15:46 +0530

    Sample Algopage Added

--- src/Components/Sample/Sample.jsx
+++ src/Components/Sample/Sample.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import '../InfoSample/InfoSample.css'
+import '../../AlgoPage.css'
 import Hero from "./hero";
+import About from "./about";
+import Aspects from "./aspects";
 
 export default function Sample() {
 return(
     <div className="Sample">
         <Hero/>
+        <About/>
+        <Aspects/>
     </div>
 )
 }
