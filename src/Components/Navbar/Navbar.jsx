88549f17b2d52fb249df863cfb7361fbf5c006ed
shreya0204 <www.shreyaagarwal.com@gmail.com>
2021-10-29 17:17:59 +0530

    imporved footer, added login button

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../Navbar/Navbar.css'
-import {Navbar, Nav, Container, NavDropdown, Button} from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown, Button } from 'react-bootstrap'
 
 
 export default function NavBar() {
@@ -8,40 +8,40 @@ export default function NavBar() {
         <div>
             <Navbar className="navbar" collapseOnSelect expand="lg" variant="light" fixed="top">
 
-            <Navbar.Brand href="/">
-                {/* <img
+                <Navbar.Brand href="/">
+                    {/* <img
                     src={logo}
                     width="50"
                     height="50"
                     className="d-inline-block align-top md:ml-5"
                     alt="Project Wing logo"
                 />{' '} */}
-            </Navbar.Brand>
-            <Navbar.Brand className="nav-name" href="/">
-            <a className="logo" href="/">
-            <img src="https://i.postimg.cc/Qxx0fCCR/Light-Theme-logo.png" alt="logo"  />
-          </a>
-                {/* <span style={{fontSize: '36px', fontFamily: 'poppins', color:'#fbda2b'}} className="nav-name">Algorithms </span>
+                </Navbar.Brand>
+                <Navbar.Brand className="nav-name" href="/">
+                    <a className="logo" href="/">
+                        <img src="https://i.postimg.cc/Qxx0fCCR/Light-Theme-logo.png" alt="logo" />
+                    </a>
+                    {/* <span style={{fontSize: '36px', fontFamily: 'poppins', color:'#fbda2b'}} className="nav-name">Algorithms </span>
                 <span style={{fontSize: '34px', fontFamily: 'poppins'}} className="nav-name">VE</span> */}
-            </Navbar.Brand>
+                </Navbar.Brand>
 
-            <Navbar.Toggle className="toggler" aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav" className="nav-links">
-                <Nav className="ml-auto" style={{fontFamily: 'Poppins'}}>
-                <Nav.Link className="nav-toggles" href="/#projects">Projects</Nav.Link>
-                <Nav.Link className="nav-toggles" href="/#about">About</Nav.Link>
-                <NavDropdown className="nav-toggles" style={{color:'#3699a1'}} title="Algorithms Used">
-                <NavDropdown.Item className="nav-toggles" href="/algo1">Algo1</NavDropdown.Item>
-                <NavDropdown.Item className="nav-toggles" href="/algo2">Algo2</NavDropdown.Item>
-                <NavDropdown.Item className="nav-toggles" href="/algo3">Algo3</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item className="nav-toggles" href="/sample">Sample</NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link className="nav-toggles" href="/#developers">Developers</Nav.Link>
-                {/* <Nav.Link className="nav-toggles" href="/#developers"><Button href="/login">Login</Button>{' '}</Nav.Link> */}
-                </Nav>
-            </Navbar.Collapse>
-            </Navbar>         
+                <Navbar.Toggle className="toggler" aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" className="nav-links">
+                    <Nav className="ml-auto" style={{ fontFamily: 'Poppins' }}>
+                        <Nav.Link className="nav-toggles" href="/#projects">Projects</Nav.Link>
+                        <Nav.Link className="nav-toggles" href="/#about">About</Nav.Link>
+                        <NavDropdown className="nav-toggles" style={{ color: '#3699a1' }} title="Algorithms Used">
+                            <NavDropdown.Item className="nav-toggles" href="/algo1">Algo1</NavDropdown.Item>
+                            <NavDropdown.Item className="nav-toggles" href="/algo2">Algo2</NavDropdown.Item>
+                            <NavDropdown.Item className="nav-toggles" href="/algo3">Algo3</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item className="nav-toggles" href="/sample">Sample</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link className="nav-toggles" href="/#developers">Developers</Nav.Link>
+                    </Nav>
+                    <Nav.Link className="nav-toggles" href="/#developers"><Button className="login_button" href="/login">Login</Button></Nav.Link>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     )
 }
