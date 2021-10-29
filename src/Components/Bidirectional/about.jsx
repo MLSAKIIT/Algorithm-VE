c1e521c3b16a0d33b4d9f9c0765cc780c592e954
atharv1703 <atharv.2001.as@gmail.com>
2021-10-29 13:59:50 +0530

    Bidirectional Update

--- src/Components/Bidirectional/about.jsx
+++ src/Components/Bidirectional/about.jsx
@@ -10,21 +10,10 @@ return(
 
       <div className="container my-5">
       <br></br>
-      <h2 className="ts">What is Sample Algorithm?</h2>
+      <h2 className="ts">What is Bidirectional Algorithm?</h2>
 
       <h4 className="my-5" id="ts-para">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem vero
-            fugit accusantium quaerat voluptatibus blanditiis quam architecto.
-            Voluptatem non laborum, consequatur, veritatis consectetur rerum
-            animi, sequi ab debitis excepturi eum deleniti nisi saepe vero
-            eligendi quibusdam enim quas quia repellat commodi. Excepturi earum
-            amet consequuntur, in natus distinctio sed repellendus! Repellat,
-            error. Ullam nulla officia hic facilis ipsam beatae quia quo
-            suscipit, at, distinctio quasi. Est laudantium quidem natus numquam
-            dolorem praesentium esse, explicabo optio harum ipsam quae sequi
-            impedit quam quisquam illum vitae perferendis. Delectus sapiente
-            unde consequatur veniam beatae labore necessitatibus placeat
-            nostrum, obcaecati illo temporibus, earum velit!
+      <b>Bidirectional search</b> is a graph search where unlike Breadth First search and Depth First Search, the search begins simultaneously from Source vertex and Goal vertex and ends when the two searches meet somewhere in between in the graph. This is thus especially used for getting results in a fraction of the time taken by both DFS and FS searches. The search from the initial node is forward search while that from the goal node is backwards. It is also based on heuristic search meaning finding the shortest path to goal optimally.
         </h4>
 
       </div>
