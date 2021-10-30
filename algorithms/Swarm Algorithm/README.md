13e202c1064b78be3b0d68d5ec1041ad91bd4e3a
Subhayan Das <nips.subhayan@gmail.com>
2021-10-30 13:53:06 +0530

    Revert "Tejash"

--- algorithms/Swarm Algorithm/README.md
+++
@@ -1,7 +0,0 @@
-Particle Swarm Optimization (PSO) is an optimization technique introduced by Kennedy and Eberhart in 1995 [39]. It uses a simple mechanism that mimics swarm behaviour in birds
-flocking and fish schooling to guide the particles to search for global optimal solutions.Many swarm optimization algorithms have been introduced since the early 60’s, Evolutio-
--nary Programming to the most recent, Grey Wolf Optimization. All of these algorithms have demonstrated their potential to solve many optimization problems. 
-This paper provides an in-depth survey of well-known optimization algorithms. Selected algorithms are briefly explained and compared with each other comprehensively through 
-experiments conducted using thirty well-known benchmark functions. Their advantages and disadvantages are also discussed. A number of statistical tests are then carried out to 
-determine the significant performances. The results indicate the overall advantage of Differential Evolution (DE) and is closely followed by Particle Swarm Optimization (PSO), 
-compared with other considered approaches.
--- src/Components/Sample/about.jsx
+++ src/Components/Sample/about.jsx
@@ -10,13 +10,10 @@ return(
 
       <div className="container my-5">
       <br></br>
-      <h2 className="ts">What is Swarm Algorithm?</h2>
+      <h2 className="ts">What is Bidirectional Swarm Algorithm?</h2>
 
       <h4 className="my-5" id="ts-para">
-      Swarm Algorithm is the collective behavior of decentralized, self-organized systems, natural or artificial.The concept is employed in work on artificial intelligence. The expression was introduced by Gerardo Beni and Jing Wang in 1989, in the context of cellular robotic systems.
-      SI systems consist typically of a population of simple agents or boids interacting locally with one another and with their environment.The inspiration often comes from nature, especially biological systems. The agents follow very simple rules, and although there is no centralized control structure dictating how individual agents should behave, local, and to a certain degree random, interactions between such agents lead to the emergence of "intelligent" global behavior, unknown to the individual agents.
-      Examples of swarm intelligence in natural systems include ant colonies, bee colonies, bird flocking, hawks hunting, animal herding, bacterial growth, fish schooling and microbial intelligence.
-      The application of swarm principles to robots is called swarm robotics while swarm intelligence refers to the more general set of algorithms. Swarm prediction has been used in the context of forecasting problems. Similar approaches to those proposed for swarm robotics are considered for genetically modified organisms in synthetic collective intelligence.
+      <b>Bidirectional search</b> is a graph search where unlike Breadth First search and Depth First Search, the search begins simultaneously from Source vertex and Goal vertex and ends when the two searches meet somewhere in between in the graph. This is thus especially used for getting results in a fraction of the time taken by both DFS and FS searches. The search from the initial node is forward search while that from the goal node is backwards. It is also based on heuristic search meaning finding the shortest path to goal optimally.
         </h4>
 
       </div>
