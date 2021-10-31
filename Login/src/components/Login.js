4ea194ccb7f1d6fd3c0bb85532007bc85023d604
SubhayanDas08 <nips.subhayan@gmail.com>
2021-10-31 19:53:51 +0530

    Minor change

--- Login/src/components/Login.js
+++ Login/src/components/Login.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import Sawo from "sawo"
-import {useState,useEffect} from "react"
-require('dotenv').config()
+// import React from 'react'
+// import Sawo from "sawo"
+// import {useState,useEffect} from "react"
+// require('dotenv').config()
 
 
-function Login() {
+// function Login() {
 
-    const [Login, setLogin] = useState(false);
+//     const [Login, setLogin] = useState(false);
     
-    useEffect(() => {
-        var config = {
-            // should be same as the id of the container created on 3rd step
-            containerID: "login-input",
-            // can be one of 'email' or 'phone_number_sms'
-            identifierType: "email",
-            // Add the API key copied from 2nd step
-            apiKey:"11e5736c-550a-467a-848f-a7c408bd417e",
-            // Add a callback here to handle the payload sent by sdk
-            onSuccess: (payload) => {},
-        };
-        let sawo = new Sawo(config);
-         sawo.showForm();
-    }, [])
+//     useEffect(() => {
+//         var config = {
+//             // should be same as the id of the container created on 3rd step
+//             containerID: "login-input",
+//             // can be one of 'email' or 'phone_number_sms'
+//             identifierType: "email",
+//             // Add the API key copied from 2nd step
+//             apiKey:"11e5736c-550a-467a-848f-a7c408bd417e",
+//             // Add a callback here to handle the payload sent by sdk
+//             onSuccess: (payload) => {},
+//         };
+//         let sawo = new Sawo(config);
+//          sawo.showForm();
+//     }, [])
 
 
-    return (<>
+//     return (<>
     
-        <div style={{
+//         <div style={{
             
-        }}>
-        <div id="login-input" style={{height:"300px", width:"400px"}}></div>
-        </div>
-        </>
-    )
-}
+//         }}>
+//         <div id="login-input" style={{height:"300px", width:"400px"}}></div>
+//         </div>
+//         </>
+//     )
+// }
 
-export default Login
+// export default Login
