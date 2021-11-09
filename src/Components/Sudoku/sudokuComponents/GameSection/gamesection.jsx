2320baaa6a66beea814abe05aafbd10d11d2f8a9
Anshul Jain <1905759@kiit.ac.in>
2021-11-09 10:36:03 +0530

    Landing Done

---
+++ src/Components/Sudoku/sudokuComponents/GameSection/gamesection.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import '../GameSection/gamesection.css'
+
+export default function GameSection() {
+    return(
+        <div className="sudokuGameSection">
+        </div>
+    )
+}
---
+++ src/Components/Sudoku/sudokuComponents/Information/infocard.js
@@ -0,0 +1,38 @@
+import $ from 'jquery';
+
+function InfoCards(){
+    $.fn.commentCards = function(){
+
+        return this.each(function(){
+      
+          var $this = $(this),
+              $cards = $this.find('.card'),
+              $current = $cards.filter('.card--current'),
+              $next;
+      
+          $cards.on('click',function(){
+            if ( !$current.is(this) ) {
+              $cards.removeClass('card--current card--out card--next');
+              $current.addClass('card--out');
+              $current = $(this).addClass('card--current');
+              $next = $current.next();
+              $next = $next.length ? $next : $cards.first();
+              $next.addClass('card--next');
+            }
+          });
+      
+          if ( !$current.length ) {
+            $current = $cards.last();
+            $cards.first().trigger('click');
+          }
+      
+          $this.addClass('cards--active');
+      
+        })
+      
+      };
+      
+      $('.cards').commentCards();
+}
+
+export {InfoCards};
