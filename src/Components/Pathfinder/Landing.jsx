9e362e7800aba78a3adb6b38fb7f924bf9c31ae0
SubhayanDas08 <nips.subhayan@gmail.com>
2021-11-11 12:53:56 +0530

    Particlejs added

--- src/Components/Pathfinder/Landing.jsx
+++ src/Components/Pathfinder/Landing.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Particle from './Particle'
 import './Pathfinder.css'
 
 export default function Landing() {
     return (
         <div className="path_landing">
+            <Particle/>
             <h1>Hello World!</h1>
         </div>
     )
---
+++ src/Components/Pathfinder/Particle.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+import particlesConfig from '../../Config/particle-config';
+
+export default function Particle(){
+    return(
+        <div>
+        <Particles         
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          height: '100vh',
+          width: '100%',
+          zIndex: 0
+          
+        }} params={particlesConfig}/>
+        </div>
+    )
+}
