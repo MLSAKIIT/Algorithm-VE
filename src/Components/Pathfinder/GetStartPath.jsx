11ddc7e6bc7ec095d418bb30f2fc296f79468107
Aditya Sinha <adityasinha361@gmail.com>
2021-11-16 19:44:10 +0530

    button added

---
+++ src/Components/Pathfinder/GetStartPath.jsx
@@ -0,0 +1,16 @@
+import "./Pathfinder.css";
+import React, { Component } from "react";
+
+export default function GetStartPath() {
+  return (
+    <div className="get_start_page">
+      <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Get Started
+      </a>
+    </div>
+  );
+}
